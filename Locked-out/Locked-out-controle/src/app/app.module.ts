e2b0354ca6f9dbb75c28ea698be46194f8b951c2
Marcelo Henrique <73535877+feitan02@users.noreply.github.com>
2023-09-05 17:12:30 -0300

    Add files via upload

---
+++ Locked-out/Locked-out-controle/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
+import { BloquearUrlComponent } from './bloquear-url/bloquear-url.component';
+import { HistoricoComponent } from './historico/historico.component';
+import { ConfiguracoesComponent } from './configuracoes/configuracoes.component';
+import { SairComponent } from './sair/sair.component';
+
+import {AppRoutingModule} from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TelaInicialComponent,
+    BloquearUrlComponent,
+    HistoricoComponent,
+    ConfiguracoesComponent,
+    SairComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
