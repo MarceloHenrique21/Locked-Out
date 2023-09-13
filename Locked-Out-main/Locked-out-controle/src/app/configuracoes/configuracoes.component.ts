462606029535e17fcb283657493c6d1d9dfb50a7
Marcelo Henrique <73535877+feitan02@users.noreply.github.com>
2023-09-13 18:41:52 -0300

    Add files via upload

---
+++ Locked-Out-main/Locked-out-controle/src/app/configuracoes/configuracoes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatRadioModule} from '@angular/material/radio';
+
+@Component({
+  selector: 'app-configuracoes',
+  templateUrl: './configuracoes.component.html',
+  styleUrls: ['./configuracoes.component.scss'],
+})
+export class ConfiguracoesComponent {
+
+  }
+
+
